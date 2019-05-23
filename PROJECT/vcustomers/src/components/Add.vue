60c664d260a4c7d03e9fcc36c790d5980ef183be
scz5221201 <527576805@qq.com>
2019-05-23 23:42:21 +0800

    导航+显示用户信息+添加用户信息

---
+++ PROJECT/vcustomers/src/components/Add.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="Add container">
+    <h1 class="page-header">添加用户</h1>
+    <form v-on:submit="addCustomers">
+      <div class="well">
+        <h4>用户信息</h4>
+        <div class="form-group">
+          <label>姓名</label>
+          <input type="text" class="form-control" placeholder="姓名" v-model="customer.name">
+        </div>
+        <div class="form-group">
+          <label>电话</label>
+          <input type="text" class="form-control" placeholder="电话" v-model="customer.phone">
+        </div>
+        <div class="form-group">
+          <label>邮箱</label>
+          <input type="text" class="form-control" placeholder="邮箱" v-model="customer.email">
+        </div>
+        <div class="form-group">
+          <label>学历</label>
+          <input type="text" class="form-control" placeholder="学历" v-model="customer.education">
+        </div>
+        <div class="form-group">
+          <label>毕业学校</label>
+          <input type="text" class="form-control" placeholder="毕业学校" v-model="customer.graduationSchool">
+        </div>
+        <div class="form-group">
+          <label>职业</label>
+          <input type="text" class="form-control" placeholder="职业" v-model="customer.profession">
+        </div>
+        <div class="form-group">
+          <label>个人简介</label>
+          <textarea class="form-control" rows="10" v-model="customer.profile"></textarea>
+        </div>
+        <button type="submit">确定</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Add',
+  data () {
+    return {
+      customer:{}
+    }
+  },
+  methods:{
+    addCustomers(e){
+      if(!this.customer.name || !this.customer.phone || !this.customer.email){
+        alert("请输入对应的信息")
+      }else{
+        let newCustomer={
+          name:this.customer.name,
+          phone:this.customer.phone,
+          email:this.customer.email,
+          education:this.customer.education,
+          graduationSchool:this.customer.graduationSchool,
+          profession:this.customer.profession,
+          profile:this.customer.profile
+        }
+        this.$http.post("http://localhost:3000/user", newCustomer)
+            .then(function(response){
+              alert("添加成功")
+              this.$router.push({path:"/"})
+            })
+        e.preventDefault();
+      }
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ PROJECT/vcustomers/src/components/Customers.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="customers container">
+    <div class="page-header">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>姓名</th>
+                    <th>电话</th>
+                    <th>邮箱</th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="customer in customers">
+                    <td>{{customer.name}}</td>
+                    <td>{{customer.phone}}</td>
+                    <td>{{customer.email}}</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customers',
+  data () {
+    return {
+      customers:[]
+    }
+  },
+  methods:{
+      fetchCustomers(){
+          this.$http.get("http://localhost:3000/user")
+          .then(function(response){
+              this.customers=response.body;
+          })
+      }
+  },
+  created(){
+      this.fetchCustomers()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
